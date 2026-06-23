b1821f2dfabd90054465476a933220e1b799e325
Jaya Pratti <jayapratti8898@gmail.com>
2026-06-23 19:26:02 +0000

    cleared issue in campaign

--- models/ECOMM/staging/stg_campaign_clicks.sql
+++ models/ECOMM/staging/stg_campaign_clicks.sql
@@ -1,11 +1,11 @@
-{{ config(materialized='view') }} 
+{{ config(materialized='view')     }}
 
 select
     click_id,
     campaign_id,
     customer_id,
     clicked_at,
-    hour(clicked_at as click_hour),
+    hour(clicked_at)    as click_hour,
     device_type,
     converted
-from {{source('marketing','campaign_clicks')}}
+from {{ source('marketing', 'campaign_clicks') }}
