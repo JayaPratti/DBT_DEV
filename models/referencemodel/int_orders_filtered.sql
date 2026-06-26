c972c479d80db374fa9051d9ee314113a2ee670a
Jaya Pratti <jayapratti8898@gmail.com>
2026-06-26 18:26:08 +0000

    commit

--- models/referencemodel/int_orders_filtered.sql
+++ models/referencemodel/int_orders_filtered.sql
@@ -1,5 +1,11 @@
-{{ config(materialized = 'view') }}
-select id as customer_id,
-first_name,
-last_name
-from {{ source('jaffle_shop','customers') }}
+{{
+config(materialized = 'ephemeral')
+}}
+
+
+
+select order_id,
+customer_id,
+order_date
+from {{ ref('stg_orders') }}
+where status!='returned'
--- models/referencemodel/stg_customers_ref.sql
+++ models/referencemodel/stg_customers_ref.sql
@@ -1,5 +1,5 @@
 {{ config(materialized = 'view') }}
 select id as customer_id,
 first_name,
-last_name--, email
-from  {{ source('jaffle_shop','customers') }}
+last_name
+from {{ source('jaffle_shop','customers') }}
